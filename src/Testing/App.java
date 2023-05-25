a98d684689ceb438fff74d18b319f1fcedd57d37
Acee169 <126034327+LengSokchhay@users.noreply.github.com>
2023-05-25 15:33:04 +0700

    IPE project

--- src/Testing/App.java
+++ src/Testing/App.java
@@ -1,19 +1,25 @@
+package Testing;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class App extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+      
+        Parent root = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
         stage.setTitle("Welcome to Library App");
-        stage.setScene(new Scene(root, 500, 500));
-        stage.show();
+        stage.setScene(new Scene(root));
+        stage.show();   
     }
+    
+
+   public static void main(String[] args) {launch(args);}
+
+
 }
